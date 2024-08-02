0c269ec2c6ec0a64fae74351d62d9a9720a3a8f7
Luiz Cavina <79873070+ZeroRaven23@users.noreply.github.com>
2024-08-02 14:48:51 -0300

    init no comebts

--- init.sh
+++ init.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-# Set environment variables
 export GOROOT=/usr/local/go
 export GOPATH=/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
-# Change to the app directory
 cd /app
 
-# Git pull with credentials
 git pull https://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@github.com/Pizzy23/qira_backend.git
 
-# Run swag init
 swag init
 
-# Build the Go application
 go build -o main .
