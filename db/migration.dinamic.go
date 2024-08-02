ba3e93e9823b12eb6d670127698093714ccf23d0
Luiz Cavina <79873070+ZeroRaven23@users.noreply.github.com>
2024-08-02 10:55:26 -0300

    Fix

--- db/migration.dinamic.go
+++ db/migration.dinamic.go
@@ -1,44 +1,44 @@
 package db
 
 type Relevance struct {
-	ID           int64  `json:"id" xorm:"pk autoincr 'id' INT"`
-	ControlID    int64  `json:"controlId" xorm:"INT notnull"`
-	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255)"`
-	Porcent      int64  `json:"porcent" xorm:"INT notnull"`
+	ID           int64  `json:"id" xorm:"pk autoincr 'id'"`
+	ControlID    int64  `json:"controlId" xorm:"INT 'control_id'"`
+	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255) 'type_of_attack'"`
+	Porcent      int64  `json:"porcent" xorm:"INT 'porcent'"`
 }
 
 type Control struct { //Control Strength
-	ID           int64  `json:"id" xorm:"pk autoincr 'id' INT"`
-	ControlID    int64  `json:"controlId" xorm:"INT notnull"`
-	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255)"`
-	Porcent      string `json:"porcent" xorm:"VARCHAR(255) notnull"`
-	Aggregate    string `json:"aggregate" xorm:"VARCHAR(255) notnull"`
-	ControlGap   string `json:"controlGap" xorm:"VARCHAR(255) notnull"`
+	ID           int64  `json:"id" xorm:"pk autoincr 'id'"`
+	ControlID    int64  `json:"controlId" xorm:"INT 'control_id'"`
+	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255) 'type_of_attack'"`
+	Porcent      string `json:"porcent" xorm:"VARCHAR(255) 'porcent'"`
+	Aggregate    string `json:"aggregate" xorm:"VARCHAR(255) 'aggregate'"`
+	ControlGap   string `json:"controlGap" xorm:"VARCHAR(255) 'control_gap'"`
 }
 
 type Propused struct { //Propused Strength
-	ID           int64  `json:"id" xorm:"pk autoincr 'id' INT"`
-	ControlID    int64  `json:"controlId" xorm:"INT notnull"`
-	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255)"`
-	Porcent      string `json:"porcent" xorm:"VARCHAR(255) notnull"`
-	Aggregate    string `json:"aggregate" xorm:"VARCHAR(255) notnull"`
-	ControlGap   string `json:"controlGap" xorm:"VARCHAR(255) notnull"`
+	ID           int64  `json:"id" xorm:"pk autoincr 'id'"`
+	ControlID    int64  `json:"controlId" xorm:"INT 'control_id'"`
+	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255) 'type_of_attack'"`
+	Porcent      string `json:"porcent" xorm:"VARCHAR(255) 'porcent'"`
+	Aggregate    string `json:"aggregate" xorm:"VARCHAR(255) 'aggregate'"`
+	ControlGap   string `json:"controlGap" xorm:"VARCHAR(255) 'control_gap'"`
 }
 
 type RelevanceDinamicInput struct {
-	ControlID    int64  `json:"controlId" xorm:"INT notnull"`
-	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255)"`
-	Porcent      int64  `json:"porcent" xorm:"VARCHAR(255)notnull"`
+	ControlID    int64  `json:"controlId" xorm:"INT 'control_id'"`
+	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255) 'type_of_attack'"`
+	Porcent      int64  `json:"porcent" xorm:"INT 'porcent'"`
 }
 
 type ControlDinamicInput struct {
-	ControlID    int    `json:"controlId" xorm:"INT notnull"`
-	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255)"`
-	Porcent      string `json:"porcent" xorm:"VARCHAR(255)notnull"`
+	ControlID    int64  `json:"controlId" xorm:"INT 'control_id'"`
+	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255) 'type_of_attack'"`
+	Porcent      string `json:"porcent" xorm:"VARCHAR(255) 'porcent'"`
 }
 
 type PropusedDinamicInput struct {
-	ControlID    int    `json:"controlId" xorm:"INT notnull"`
-	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255)"`
-	Porcent      string `json:"porcent" xorm:"VARCHAR(255)notnull"`
+	ControlID    int64  `json:"controlId" xorm:"INT 'control_id'"`
+	TypeOfAttack string `json:"type_of_attack" xorm:"VARCHAR(255) 'type_of_attack'"`
+	Porcent      string `json:"porcent" xorm:"VARCHAR(255) 'porcent'"`
 }
